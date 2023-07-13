5aa5f28cfdd4256a255f667e503059668afa816f
Van Noel <vannoel0628@gmail.com>
2023-07-13 22:00:29 +0800

    init developing env of frontend

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # course-202307-php-rwd
 Project of course to laravel and RWD.
+
+## Docker
+
+### install
+
+Install [docker](https://www.docker.com/) and docker compose.
+
+(Some OS might require to install compose addionally.)
+
+### Run up
+
+To root of projecy, which means `here`.
+
+```
+docker compose up
+```
+
+### Run down 
+
+> where to call up then where to call down
+
+```
+docker compose down
+```
+
+## Backend ( Php )
+
+### Install
+
+Only install [PHP](https://www.php.net/) when you need to execute php directly.
+
+## Frontend ( Javascript )
+
+### Install
+
+Only install [Nodejs](https://nodejs.org/) whne you need to execute Node Package Manager, [NPM](https://www.npmjs.com/).
+
+If you need to switch node version, please install Node Version Manager, [NVM](https://github.com/nvm-sh/nvm).
+
+If you need to develop, you can install some IDE (Integrated Development Environment) such like [VScode](https://code.visualstudio.com/).
+
+### Wndows
+
+If windows can not found Nodejs, please refer the following pages:
+
+Solution to nodejs is not found in windows: [中文](https://www.jianshu.com/p/6cda6ca6cce1) and [English](https://stackoverflow.com/questions/27864040/fixing-npm-path-in-windows-8-and-10)
--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,12 +1,6 @@
 const gulp = require('gulp');
-const del = require('del');
-
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
 
 const path_libs = './libs'
-const path_homepage = '../blacktoolboxlaboratory.github.io';
-const path_backup = '../codebase/homepage';
 
 gulp.task('copyVendor', function(done) { 
   /* jQuery */
@@ -18,64 +12,16 @@ gulp.task('copyVendor', function(done) {
     .pipe(gulp.dest(path_libs+'/fontawesome/css'));
   gulp.src(['node_modules/@fortawesome/fontawesome-free/webfonts/**/*'])
     .pipe(gulp.dest(path_libs+'/fontawesome/webfonts'));
+
+  /* swiper */
+  gulp.src(['node_modules/swiper/swiper-bundle.min.css'])
+    .pipe(gulp.dest(path_libs+'/swiper/'));
+  gulp.src(['node_modules/swiper/swiper-bundle.min.js'])
+    .pipe(gulp.dest(path_libs+'/swiper/'));
+
+  /* gsap */
+  gulp.src(['node_modules/gsap/dist/gsap.min.js'])
+    .pipe(gulp.dest(path_libs+'/gsap/'));
   
-  done();
-});
-
-gulp.task('updateHomePage', function(done){
-  /* html & ico*/
-  gulp.src(['index.html', 'favicon.ico'])
-    .pipe(gulp.dest(path_homepage));
-
-  /* style */
-  gulp.src(['style/index.css'])
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(autoprefixer('last 2 versions'))
-    .pipe(gulp.dest(path_homepage+'/style'))
-  
-  /* font */
-  gulp.src(['style/google-fonts/*'])
-    .pipe(gulp.dest(path_homepage+'/style/google-fonts/'))
-
-  /* libs */
-  gulp.src(['libs/**/*'])
-    .pipe(gulp.dest(path_homepage+'/libs/'))
-  
-  /* img */
-  gulp.src(['img/**/*'])
-    .pipe(gulp.dest(path_homepage+'/img/'))
-
-  /* og:image */
-  gulp.src(['logo_btb.png']).pipe(gulp.dest(path_homepage));
-  
-  /* sitemap */
-  gulp.src(['sitemap.xml'])
-    .pipe(gulp.dest(path_homepage))
-
-  done();
-});
-
-gulp.task('backupCodebase', async function(done) {  
-  /* clean files */
-  await del([path_backup + '/'], {force: true})
-
-  /* src */
-  gulp.src(['img/*'])
-    .pipe(gulp.dest(path_backup + '/img/'));
-  gulp.src(['style/**/*'])
-    .pipe(gulp.dest(path_backup + '/style/'));
-  /* others */        
-  gulp.src([
-      '.gitignore',
-      'LICENSE',
-      'README.md',
-      'package.json',
-      'package-lock.json',
-      'gulpfile.js',
-      'index.html',
-      'favicon.ico',
-      'sitemap.xml'
-    ])
-    .pipe(gulp.dest(path_backup));
   done();
 });
